87f507af77ff27bbcb2b162dc122ffa25a39690c
zhz8888 <zhz1021@gmail.com>
2025-12-31 00:51:54 +0800

    ci(workflow): 优化GitHub Actions配置并移除git clone的深度限制
    移除diy-part2.sh中git clone的--depth=1参数以获取完整提交历史
    在immortalwrt.yml中添加空行提升可读性

--- diy-part2.sh
+++ diy-part2.sh
@@ -48,17 +48,17 @@ CONFIG_QCOM_IPA=m
 EOF
 
 # HelmiWrt packages
-git clone --depth=1 https://github.com/Haris131/helmiwrt-packages package/community
+git clone https://github.com/Haris131/helmiwrt-packages package/community
 
 # 自定义第三方插件
-git clone --depth=1 https://github.com/chenmozhijin/luci-app-socat.git package/luci-app-socat
-# git clone --depth=1 https://github.com/qianlyun123/luci-app-modem.git package/luci-app-modem
-# git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
-git clone --depth=1 https://github.com/linkease/istore.git package/istore
-git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
+git clone https://github.com/chenmozhijin/luci-app-socat.git package/luci-app-socat
+# git clone https://github.com/qianlyun123/luci-app-modem.git package/luci-app-modem
+# git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
+git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
 
 # 410棒子专属插件
-git clone --depth=1 https://github.com/lkiuyu/luci-app-cpu-perf package/luci-app-cpu-perf
-git clone --depth=1 https://github.com/lkiuyu/luci-app-cpu-status package/luci-app-cpu-status
-git clone --depth=1 https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-status
-git clone --depth=1 https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
+git clone https://github.com/lkiuyu/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/lkiuyu/luci-app-cpu-status package/luci-app-cpu-status
+git clone https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-status
+git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
